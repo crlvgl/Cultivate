08f1c684434187340f4171ccc15e4aa7ff3e64d1
Julimaeder <maeder.julian@gmail.com>
2024-01-19 18:46:10 +0100

    Added a slider for the player control steps

---
+++ Assets/Scripts/Glaubensanzeige.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glaubensanzeige : MonoBehaviour
+{
+    private GameObject imageRelic;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageRelic = transform.Find("ImageRelic").gameObject;
+        if (imageRelic != null)
+        {
+            imageRelic.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Inventory.Relic == 1)
+        {
+            imageRelic.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/GlaubensanzeigeSlider.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlaubensanzeigeSlider : MonoBehaviour
+{
+    public float GlaubensanzeigeTargetValue = 0f;
+    private Slider slider;
+    private GameObject fillGameObject;
+    private float FillSpeed = 0.1f;
+    private ParticleSystem ParticleSys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+// Get the Slider component
+        slider = GetComponent<Slider>();
+        ParticleSys = GameObject.Find("SliderParticles").GetComponent<ParticleSystem>();
+
+        // Find the "Fill" child GameObject
+        fillGameObject = transform.Find("Fill Area").gameObject;
+
+        // Deactivate the "Fill" child GameObject
+        if (fillGameObject != null)
+        {
+            fillGameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlaubensanzeigeTargetValue > 0)
+        {
+            fillGameObject.SetActive(true);
+        }
+        if (GlaubensanzeigeTargetValue > slider.value)
+        {
+            slider.value += Time.deltaTime * FillSpeed;
+            if (!ParticleSys.isPlaying)
+                ParticleSys.Play();
+        }
+        else
+        {
+            ParticleSys.Stop();
+        }
+
+        if(Inventory.Relic == 1)
+        {
+            GlaubensanzeigeTargetValue = 0.5f;
+        } 
+        if(Inventory.Altar == 1)
+        {
+            GlaubensanzeigeTargetValue = 1f;
+        } 
+    }
+}
