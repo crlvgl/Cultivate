8f6db771bb585d6f26b7d85ff84b779f0bd2f7bc
Carl Vogel <vogel.carl.l@gmail.com>
2024-03-07 22:23:59 +0100

    added return to Main Screen after game is done

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -7,9 +7,11 @@ public class EndGame : MonoBehaviour
     public Animator animator;
     public float waitBeforeEndGame1 = 2f;
     public float waitBeforeEndGame2 = 4.5f;
+    public float waitBeforeEndGame3 = 3f;
     public static bool disableAll = false;
     public static bool moveToP1 = false;
     public static bool moveToP2 = false;
+    public string pathToScene;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,5 +46,13 @@ public class EndGame : MonoBehaviour
         animator.enabled = true;
         yield return new WaitForSeconds(6.010f);
         animator.enabled = false;
+        yield return new WaitForSeconds(waitBeforeEndGame3);
+
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(pathToScene);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
